e4e2eeaa74e9234b5103e5e4aaa794aac3ad9d3b
Hou Yueyuan <houyy1990@163.com>
2025-11-03 15:46:54 +0800

    seo(robots): allow site sections and disallow /api, /_next, /download, query URLs; keep sitemap reference

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,13 @@ import { siteConfig } from '@/lib/siteConfig';
 export default function robots(): MetadataRoute.Robots {
   const url = siteConfig.url;
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/en/*', '/zh/*', '/guides/*', '/templates', '/tool'],
+        disallow: ['/api/*', '/_next/*', '/download/*', '/*?*'],
+      },
+    ],
     sitemap: `${url}/sitemap.xml`,
     host: url,
   };
